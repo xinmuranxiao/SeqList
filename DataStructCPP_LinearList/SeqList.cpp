85a4c1830c6a0c46e479b9dcda1e05b4ad981991
xinmuranxiao <xzt98948364@outlook.com>
2026-03-14 13:37:49 +0800

    添加项目文件。

---
+++ DataStructCPP_LinearList/SeqList.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+template<class T>
+class SeqList {
+public:
+	SeqList(int size_);
+	
+	int getSize();
+	int getCapacity();
+	T& operator[](pos) {
+		if (idx < 0 || idx >= arr.size()) {
+			throw std::out_of_range(
+				"下标越界！有效范围：0~" + std::to_string(arr.size() - 1) +
+				"，你输入的下标：" + std::to_string(idx)
+			);
+		}
+
+	}
+	
+	~SeqList();
+private:
+	int size = 0;
+	int capacity = 0;
+	vector<T> arr;
+};
+
+template<class T>
+SeqList<T>::SeqList(int size_) {
+	if (size_ <= 0) {
+		throw invalid_argument("1");
+	}
+	arr.reserve(size_);
+	size = size_;
+}
+
+template<class T>
+int SeqList<T>::getSize() {
+	return size;
+}
+
+template<class T>
+int SeqList<T>::getCapacity() {
+	return capacity;
+}
+
+template<class T>
+SeqList<T>::~SeqList() {
+}
